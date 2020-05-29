717b2286a4ec08c8153d32263a241c1132e3ed24
Michał Leszczyński <ml@icedev.pl>
2020-05-29 23:40:55 +0200

    [SANDBOX] Patch qemu build for Ubuntu Focal

--- package/patch-xen.sh
+++ package/patch-xen.sh
@@ -10,3 +10,19 @@ fi
 
 echo 'Patched qemu to include --disable-sdl option'
 sed -i 's#source/configure --enable-xen --target-list=i386-softmmu#source/configure --enable-xen --target-list=i386-softmmu --disable-sdl#g' tools/Makefile
+
+DISTRIBUTION=$(lsb_release -cs)
+
+if [ "$DISTRIBUTION" = "focal" ]
+then
+    # until we bump Xen, we need to avoid -Werror in qemu
+    echo 'Patching tools/Makefile to add --disable-werror'
+    sed -i 's#--disable-guest-agent #--disable-guest-agent --disable-werror #g' tools/Makefile
+
+    if [ $? -ne 0 ]
+    then
+        echo 'Failed to patch tools/Makefile to add --disable-werror'
+        exit 1
+    fi
+fi
+
