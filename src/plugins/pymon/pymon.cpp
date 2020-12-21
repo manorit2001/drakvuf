2563d032362088a084545f8b1218e9c1d5f916e2
Adam Kliś <bonus@cert.pl>
2020-12-21 19:42:33 +0100

    fix

--- src/plugins/pymon/pymon.cpp
+++ src/plugins/pymon/pymon.cpp
@@ -138,14 +138,8 @@ static event_response_t init_scripts(drakvuf_t drakvuf, drakvuf_trap_info_t* inf
 pymon::pymon(drakvuf_t drakvuf, const pymon_config& config, output_format_t output)
     : pluginex(drakvuf, output), scripts_dir{config.pymon_dir}
 {
-    drakvuf_trap_t inject_trap =
-    {
-        .type = REGISTER,
-        .reg = CR3,
-        .name = "pymon_cr3",
-        .data = static_cast<void*>(this),
-        .cb = scripts_dir == "" ? &init_scripts : &repl_start,
-    };
+    this->inject_trap.data = static_cast<void*>(this),
+    this->inject_trap.cb = scripts_dir == "" ? &init_scripts : &repl_start,
 
     if(!drakvuf_add_trap(drakvuf, &inject_trap))
         throw -1;
